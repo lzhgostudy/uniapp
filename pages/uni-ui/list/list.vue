af1217706c5a67321028161a37ca5cf678043ad7
244_100007713101_luzhihua <244_100007713101_luzhihua@git.cloud.tencent.com>
2021-03-16 07:06:31 +0800

    add uview

---
+++ pages/uni-ui/list/list.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+	.container 
+		uni-list
+		<!-- 基本用法 -->
+			uni-list-item(title="列表文字")
+			uni-list-item(title="列表禁用状态" :disabled="true")
+		<!-- 多行内容显示 -->	
+			uni-list-item(title="列表文字" note="列表描述信息")
+			uni-list-item(:disabled="true" title="列表文字" note="列表禁用状态")
+		<!-- 右侧显式角标, switch -->
+			uni-list-item(title="列表右侧显示角标" :show-badge="true" badge-text="12")
+			uni-list-item(title="列表右侧显示switch" :show-switch="true" @switchChange="switchChange")
+		<!-- 左侧显示缩略图,图标 -->
+			uni-list-item(title="列表左侧带缩略图" note="列表描述信息" :thumb="thumb" thumb-size="lg" rightText="右侧文字")
+			<!-- uni-list-item(title="列表左侧带扩展图标" :show-extra-icon="true" extra-icon="contact") -->
+		<!-- 开启点击反馈和右侧箭头 -->
+			uni-list-item(title="开启点击反馈" clickable @click="onClick")
+			uni-list-item(title="默认 navigateTo 方式跳转页面" link to="/pages/index/index" @click="onClick($event, 2)")
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			thumb: 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-dc-site/460d46d0-4fcc-11eb-8ff1-d5dcf8779628.png'
+		}
+	},
+	methods: {
+		switchChange(e) {
+			console.log('switch value:', e.value)
+		},
+		onClick(e) {
+			console.log('开启点击反馈:', e)
+		}
+	}
+} 
+</script>
+
+<style>
+</style>
---
+++ pages/uview/componentsA/form/index.vue
@@ -0,0 +1,54 @@
+<template lang="pug">
+	.wrap
+		u-form(:model="model" ref="uForm")
+			u-form-item(label="结算方式")
+				u-radio-group(v-model="radio" @change="radioGroupChange")
+					u-radio(v-for="(item, index) in radioList" :key="index" :name="item.value") {{ item.name }}
+</template>
+
+<script>
+	export default{
+		data() {
+			return {
+				radio: 'alipay',
+				radioList: [
+					{
+						name: '支付宝',
+						value: 'alipay',
+						checked: true,
+						disabled: false
+					},
+					{
+						name: '微信',
+						value: 'wechat',
+						checked: false,
+						disabled: false
+					},
+					{
+						name: '银联',
+						value: 'union',
+						checked: false,
+						disabled: false
+					},
+					{
+						name: '现金',
+						value: 'cash',
+						checked: false,
+						disabled: false
+					}
+				],
+				model: {
+					
+				}
+			}
+		},
+		methods: {
+			radioGroupChange(e) {
+				console.log('radioGroupChange:', e)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
